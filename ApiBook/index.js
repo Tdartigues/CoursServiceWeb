7db93d0da284c8f96f28c1e689d3f7854802d0d2
Tdartigues <73830061+Tdartigues@users.noreply.github.com>
2021-02-08 17:44:49 +0100

    Add Post and Delete

--- ApiBook/index.js
+++ ApiBook/index.js
@@ -5,7 +5,10 @@ const fs = require('fs')
 
 app.use(express.json())
 
-const getBooks = () => require('../Books.json')
+const getBooks = () => {
+    purgeCache('../Books.json')
+    return require('../Books.json')
+}
 
 const findBook = (id) => getBooks().find(m => m.id == id)
 
@@ -37,3 +40,72 @@ app.put('/books/:id', (req, res) => {
         res.status(404).end()
     }
 })
+
+app.post('/books', (req, res) => {
+    let books = getBooks()
+    let newBook = req.body
+    if(books.find(b => b.id == newBook.id)){
+        res.status(400).end()
+    } else {
+        books.push(newBook)
+        fs.writeFileSync(__dirname + '\\..\\Books.json', JSON.stringify(books))
+        res.status(201).send(newBook)
+    }
+})
+
+app.delete('/books/:id', (req, res) => {
+    let books = getBooks()
+    let bookID = req.params.id
+    if(books.find(b => b.id == bookID)){
+        const nBooks = books.filter(b => b.id != bookID)
+        fs.writeFileSync(__dirname + '\\..\\Books.json', JSON.stringify(nBooks))
+        res.status(204).end()
+    } else {
+        res.status(404).end()
+    }
+})
+
+/**
+ * Removes a module from the cache
+ */
+function purgeCache(moduleName) {
+    // Traverse the cache looking for the files
+    // loaded by the specified module name
+    searchCache(moduleName, function (mod) {
+        delete require.cache[mod.id];
+    });
+
+    // Remove cached paths to the module.
+    // Thanks to @bentael for pointing this out.
+    Object.keys(module.constructor._pathCache).forEach(function(cacheKey) {
+        if (cacheKey.indexOf(moduleName)>0) {
+            delete module.constructor._pathCache[cacheKey];
+        }
+    });
+};
+
+/**
+ * Traverses the cache to search for all the cached
+ * files of the specified module name
+ */
+function searchCache(moduleName, callback) {
+    // Resolve the module identified by the specified name
+    var mod = require.resolve(moduleName);
+
+    // Check if the module has been resolved and found within
+    // the cache
+    if (mod && ((mod = require.cache[mod]) !== undefined)) {
+        // Recursively go over the results
+        (function traverse(mod) {
+            // Go over each of the module's children and
+            // traverse them
+            mod.children.forEach(function (child) {
+                traverse(child);
+            });
+
+            // Call the specified callback providing the
+            // found cached module
+            callback(mod);
+        }(mod));
+    }
+};
--- ApiMagasine/index.js
+++ ApiMagasine/index.js
@@ -5,7 +5,10 @@ const port = 3001
 
 app.use(express.json())
 
-const getMagazines = () => require('../Magazine.json')
+const getMagazines = () => {
+    purgeCache('../Magazine.json')
+    return require('../Magazine.json')
+}
 
 const findMagazines = (id) => getMagazines().find(m => m.id == id)
 
@@ -38,3 +41,73 @@ app.put('/magazines/:id', (req, res) => {
     }
 })
 
+app.post('/magazines', (req, res) => {
+    let magazines = getMagazines()
+    let newMagazine = req.body
+    if(magazines.find(m => m.id == newMagazine.id)){
+        res.status(400).end()
+    } else {
+        magazines.push(newMagazine)
+        fs.writeFileSync(__dirname + '\\..\\Magazine.json', JSON.stringify(magazines))
+        res.status(201).send(newMagazine)
+    }
+})
+
+app.delete('/magazines/:id', (req, res) => {
+    let magazines = getMagazines()
+    let magazineID = req.params.id
+    console.log(magazines)
+    console.log(magazineID)
+    if(magazines.find(m => m.id == magazineID)){
+        const nMagazines = magazines.filter(m => m.id != magazineID)
+        fs.writeFileSync(__dirname + '\\..\\Magazine.json', JSON.stringify(nMagazines))
+        res.status(204).end()
+    } else {
+        res.status(404).end()
+    }
+})
+
+/**
+ * Removes a module from the cache
+ */
+function purgeCache(moduleName) {
+    // Traverse the cache looking for the files
+    // loaded by the specified module name
+    searchCache(moduleName, function (mod) {
+        delete require.cache[mod.id];
+    });
+
+    // Remove cached paths to the module.
+    // Thanks to @bentael for pointing this out.
+    Object.keys(module.constructor._pathCache).forEach(function(cacheKey) {
+        if (cacheKey.indexOf(moduleName)>0) {
+            delete module.constructor._pathCache[cacheKey];
+        }
+    });
+};
+
+/**
+ * Traverses the cache to search for all the cached
+ * files of the specified module name
+ */
+function searchCache(moduleName, callback) {
+    // Resolve the module identified by the specified name
+    var mod = require.resolve(moduleName);
+
+    // Check if the module has been resolved and found within
+    // the cache
+    if (mod && ((mod = require.cache[mod]) !== undefined)) {
+        // Recursively go over the results
+        (function traverse(mod) {
+            // Go over each of the module's children and
+            // traverse them
+            mod.children.forEach(function (child) {
+                traverse(child);
+            });
+
+            // Call the specified callback providing the
+            // found cached module
+            callback(mod);
+        }(mod));
+    }
+};
