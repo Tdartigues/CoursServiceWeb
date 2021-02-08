8c6faafedfc337a1d3d017f1e32d3f6a3a90ebf9
Tdartigues <73830061+Tdartigues@users.noreply.github.com>
2021-02-08 09:34:26 +0100

    Start projet

--- ApiBook/ApiBook.js
+++ ApiBook/ApiBook.js
@@ -2,14 +2,14 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-const books = require('./books.json')
+const books = require('../BDD.json')
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
 app.listen(port, () => {
-  console.log('Example app listening at http://localhost:3000/%27)
+  console.log('Example app listening at http://localhost:3000/%27')
 })
 
 app.get('/books', (req, res) => {
